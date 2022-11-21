6b880486b87ce5846ddae402237f7cb612a7619e
Arick <hey@arick.io>
2022-11-21 14:28:47 -0800

    v0.1

---
+++ README.md
@@ -0,0 +1,17 @@
+# ProbeWatch
+
+Quick and easy Python script to monitor for WiFi probe request frames.
+
+## CLI
+Run with:
+```python
+python probewatch.py -h
+	usage: probewatch.py [-h] [-u] [-i INTERFACE]
+
+	options:
+  	-h, --help            show this help message and exit
+  	-u, --update          Update MAC address database
+  	-i INTERFACE, --interface INTERFACE
+                        Interface to use. Must be in monitor mode
+
+```
---
+++ probewatch.py
@@ -0,0 +1,49 @@
+from scapy.all import *
+from threading import Thread
+from mac_vendor_lookup import MacLookup, BaseMacLookup
+from progress.spinner import Spinner
+import argparse
+import pandas
+import time
+import os
+
+BaseMacLookup.cache_path = "probe_cache"
+networks = pandas.DataFrame(columns=["MAC","SSID","Vendor", "BSSID"])
+networks.set_index("MAC", inplace=True)
+
+def callback(packet):
+    if packet.haslayer(Dot11ProbeReq) and packet.subtype == 4 and len(packet.info.decode()) > 0:
+        mac = packet.addr2.upper()
+        ssid = packet.info.decode()
+        bssid = packet.addr3.upper()
+        try:
+        	vendor = MacLookup().lookup(mac)
+        except:
+        	vendor = 'n/a'
+        networks.loc[mac] = (ssid,vendor,bssid)
+
+
+def print_all():
+    while True:
+        os.system("clear")
+        print(networks)
+        time.sleep(1)
+        
+def check_for_updates():
+    print("Please wait while database is updated")
+    MacLookup().update_vendors()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--update', action="store_true", help="Update MAC address database")
+    parser.add_argument('-i','--interface', default="wlan0", help="Interface to use. Must be in monitor mode") 
+    args = parser.parse_args()
+    if args.update:
+        check_for_updates()
+    
+
+    log = Thread(target=print_all)
+    log.daemon = True
+    log.start()
+
+    sniff(prn=callback, iface=args.interface)
